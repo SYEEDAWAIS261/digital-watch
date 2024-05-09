0fe85313d7db241f4b2ca8edf5118ac846e356c3
SYEEDAWAIS261 <awaisshah14101@icloud.com>
2024-05-09 08:32:40 +0500

    add first commit

---
+++ app.js
@@ -0,0 +1,479 @@
+// // var country="pakistan";
+// // console.log(country);
+// // // var continent="Asia";
+// // // console.log(continent);
+// // // var population= "23 million";
+// // // console.log(population);
+// // // let island=false
+//  let pakistan=true
+// // // let china=false
+// // // console.log(china,island,pakistan);
+// let firstname="syeed";
+// let lastname="awais";
+// let emailaddress="awaisshah14101@icloud.com"
+// console.log(firstname,lastname);
+// console.log(emailaddress);
+// alert("welcome to our site!");//
+// let num1 = prompt("please input the first number");
+// let num2 = prompt("please input the second number");
+// let res = num1 / num2;
+// alert(res);
+// let pakistan= "10";
+// console.log(pakistan / 2);
+
+// let country= 100;
+// console.log(country + 1);
+
+// let num4=6;
+// console.log(num++);
+// console.log(num4);
+
+// let num5=10;
+// console.log(num--);
+// console.log(num5);
+
+// let num1=prompt("please input the first number");
+// let num2=prompt("please input the second number");
+// let res=num1/num2;
+// alert(res);
+// let num=10;
+// console.log(num++);
+// console.log(num);
+// let num3=3;
+// console.log(num3--);
+// console.log(num3);
+  
+// let c=6;
+// let d=8;
+// let ans= c + d * d + c - c / c -d
+// console.log(ans);
+// let firstName="syeed";
+// let lastName="SwaiS";
+// let total=`i am a student of smit ${firstName} ${lastName}`;
+// console.log(total);
+
+// let firstname= prompt('please input your firstname');
+// let lastname= prompt('please input your last name');
+// let totl=`my name is ${firstname} ${lastname}`;
+// console.log(totl);
+
+    //  let a=5;
+    //  let b=10;
+    //  let res=a == b;
+    //  console.log(res);
+     
+    //  let c=10;
+    //  let d=10;
+    //  let result=c !== b;
+    //  console.log(result);
+    //  let ab=10;
+    //  let ba=10;
+    //  let resu=ab >= ba;
+    //  console.log(resu); 
+    // let res=prompt("please input your %");
+    // if(res >= 90)
+    // {
+    //     console.log('A+ grade');
+    // }
+    // else if (res >=80)
+    // {
+    // console.log('A grade');
+    // }
+    // else if (res >=70)
+    // {
+    // console.log('B grade');
+    // }
+    // else if (res >= 60)
+    // {
+    //     console.log('C grade');
+    // }
+    // else if (res >= 50)
+    // {
+    //     console.log('D grade');
+    // }
+    // else  {
+    //     console.log('fail');
+    // }
+
+   
+// let isheight= prompt("please enter your height")
+// let Isedu=prompt("please input your qualification")
+// if(isheight > 5.5 && Isedu==true){
+//     console.log("egligible for commissioner")    
+// }
+// else if(isheight > 5.5 || Isedu ==false){
+//     console.log("egligible for soilder")
+// }
+// else{
+//     console.log("not egligible")
+// }
+
+// let speed= prompt("please input your speed")
+// let height=prompt("please input your height")
+// if(speed > 130 && height > 5.8){
+//     console.log("fast bowler")
+// }
+// else if(speed < 100 && height > 5.8){
+//     console.log("medium bowler")
+// }
+// else {
+//     console.log("sorry go to home ")
+// }
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+
+// let dis=prompt("please input your distance")
+// if(dis >=50){
+//     alert(`55 rupees will be charge form u`)
+// }
+// else if(dis >=45){
+//     alert(`50 rupees will be charge`)
+// }
+// else if(dis >=40){
+//     alert(`45 to 50 rupees will be charged form your card`)
+// }
+// else if(dis >=35){
+//     alert(`40 to 45 rupees will be charged form your card`)
+// }
+// else if(dis >=30){
+//     alert(`35 to 40 rupees will be charged form your card`)
+// }
+// else if(dis >=25){
+//     alert(`30 to 40 rupees will be charged form your card`)
+// }
+// else if(dis >=20){
+//     alert('25 to 30 rupees will be charged form your card')
+// }
+// else if(dis >=15){
+//     alert('20 to 25 rupees will be charged form your card')
+// }
+// else if(dis >=10){
+//     alert('15 to 20 rupees will be charged form your card')
+// }
+// else if(dis >=5){
+//     alert('10 to 15 rupees will be charged form your card')
+// }
+
+// let arr= [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr);
+// let kpk=["swat", "kohat", "mardan", "peshawar", "hangu"];
+// console.log("I live in "+ kpk[3]);
+
+// let fruit=["banana", "apple", "mango"]
+// fruit.push("straberry");
+// fruit.push("orange");
+// fruit.pop();
+// console.log(fruit);
+// let a=[
+//     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+//     [1, 2, 3, 4, 15, 6, 7, 8, 9],
+//     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+//     ];
+//     let b=[
+//         [12, 22, 23, 42, 52, 63, 73, 83, 93],
+//         [12, 22, 23, 42, 92, 63, 73, 83, 93],
+//         [12, 22, 23, 42, 52, 63, 73, 83, 93],
+//     ];
+
+//     console.log(a[1][4]+b[1][4]);
+
+// const dateandtime="11:00am sunday 28th january, 2024";
+// console.log(dateandtime);
+/*1*/   // var country="Pakistan";
+// console.log(country);
+// var continent="Asia";
+// console.log(continent);
+
+/*2*/  // const pop= 25;
+// console.log(pop);
+// let island=false;
+// let Pakistan=true;
+// let Indai=false;
+// let urdu= true;
+// console.log(island, Pakistan, urdu, Indai);
+/*3*/  // const PI=3.1468;
+// let radius=5;
+// console.log(PI*radius);
+
+// var lang= "urdu";
+// /*4*/  let pop=prompt("please input your population");
+// let avg=prompt("please input youe avg");
+// if(pop>6 && avg>33){
+//     console.log("yes our population strength and avg is more than fineland.");
+// }
+// else if(pop==6 && avg==33){
+//     console.log("our population and avg has same.");    
+// }
+// else{
+//     console.log("no our population strength and avg is less than fineland avg.");
+// }
+
+// /*5*/  const country="portugal";
+//        const continent="europe";
+//        const population=11;
+       
+//        const description=`${country} is a country of ${continent}
+//         continent and its population is ${population} million.`;
+//        console.log(description);
+// let arr=[5,2,5,8,6,5,5,6,96,5,55,5]
+// for (let i=0;i<=arr.lenth;i++){
+//     console.log(arr);
+// }
+// let ar=["cat","dog","box","apple"];
+// for(i=0;i<=0;i++){
+//     console.log(ar[3])
+// }
+// for(let i=1;i<=10;i++){
+//     for(let x=1;x<=10;x++){
+//         console.log(` ${i} x ${x}= ${i*x}`);
+//     }
+// }
+
+// for(a=1000;a>=0;a--){
+//         console.log(a);
+//     }
+
+// let text="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// console.log(text.length);
+// console.log(text.charAt(26));
+
+//  let num=Math.random();
+//  let score= num * 6 + 1;
+// let res= (Math.trunc(score));
+// if(res==1){
+// alert("out");
+// }
+// else if(res==6){
+// alert("won");
+// }
+// else{
+//     alert("please try again");
+// }
+
+// let number=Math.random();
+// let scor=number * 6 + 1;
+// let resu=prompt((Math.trunc.scor));
+// if(resu==6){
+//     alert("six");
+// }
+// else if(resu==5){
+//     alert("five");
+// }
+// else if(resu==4){
+//     alert("four");
+// }
+// else if(resu==3){
+//     alert("three");
+// }
+// else if(resu==2){
+//     alert("two");
+// }
+// else if(resu==1){
+//     alert("one");
+// }
+// else("try again");
+// console.log(Math.round(3.9));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(5.9));
+// console.log(Math.pow(4,3));
+// console.log(Math.sqrt(2));
+// let x=20
+// console.log(x.toString()+2);
+
+// let a="30";
+// console.log(a+10);
+// let scor=6*1;
+// let resu=prompt((Math.trunc.scor));
+// if(resu==6){
+//     alert("won");
+// }
+// else{
+//     alert("lose");
+// }
+
+// let thedate= new Date();
+// console.log(thedate);
+//  var rightNow= new Date();
+//  var theday=rightNow.getDay();
+//  console.log(theday);
+
+//  let rightNow=new Date();
+//  let theday=rightNow.getMonth();
+//  console.log(theday);
+// document.querySelector(".h1").textContent=theday;
+
+
+// let rightNow2= new Date()
+// let theyear=rightNow2.getFullYear();
+// console.log(theyear);
+// document.querySelector(".h2").textContent=theyear;
+
+
+// let rightNow1=new Date ();
+// minisecond=rightNow1.getTime();
+// console.log(minisecond);
+// document.querySelector(".h3").textContent=minisecond;
+
+// var doomsday= new Date ("June 30, 2035");
+//  console.log(doomsday);
+
+// let h1 = document.querySelector(".h1")
+
+// function button(){
+//     h1.style.backgroundColor="red"
+// }
+
+// let addition=function (num1, num2){
+//     let res= num1 + num2;
+//     return res;
+// }
+// console.log(addition(5,5))
+
+// let addition= (num1, num2)=>{
+//     return num1 * num2;
+// }
+// console.log(addition(5,5))
+
+// let mod=(num1, num3)=>{
+//     let res= num1 % num3
+//     return res
+// }
+// console.log(mod(20,40))
+
+// function addition(num1){
+//     let res=num1*num1*num1;
+//     return res
+// }
+// console.log(addition(4))
+
+// {
+// let b=10
+// console.log(b)
+// const c=15
+// console.log(c)
+// }
+
+
+// setInterval(showTime, 1000);
+// function showTime() {
+//     let time = new Date();
+//         let hour = time.getHours();
+//         let min = time.getMinutes();
+//         let sec = time.getSeconds();
+//         am_pm = "AM";
+
+
+//     if (hour >= 12) {
+//         if (hour > 12) hour -= 12;
+//         am_pm = "PM";
+//     } else if (hour == 0) {
+//         hr = 12;
+//         am_pm = "AM";
+//     }
+
+//     hour =
+//         hour < 10 ? "0" + hour : hour;
+//     min = min < 10 ? "0" + min : min;
+//     sec = sec < 10 ? "0" + sec : sec;
+
+//     let currentTime =
+//         hour +
+//         ":" +
+//         min +
+//         ":" +
+//         sec +
+//         am_pm;
+
+//     document.getElementById(
+//         "digital watch"
+//     ).innerHTML = currentTime;}
+
+// showTime();
+
+
+// let rightNow= new Date ()
+// let thehours=rightNow.getHours()
+// console.log(thehours)
+// document.querySelector(".h1a").textContent=thehours;
+
+// let rightnow1= new Date()
+// let theminits=rightnow1.getMinutes()
+// console.log(theminits)
+// document.querySelector(".h2a").textContent=theminits;
+
+// let rightNow2= new Date()
+// let thesecond=rightNow2.getSeconds()
+// console.log(thesecond)
+// document.querySelector(".h3a").textContent=thesecond;
+
+// let rightNow3=new Date()
+// let theday=rightNow3.getDay()
+// console.log(theday)
+// document.querySelector(".h4a").textContent=theday;
+
+// let rightNow4=new Date()
+// let themonth=rightNow4.getMonth()
+// console.log(themonth)
+// document.querySelector(".h5a").textContent=themonth;
+
+// let rightNow5=new Date()
+// let theyear=rightNow5.getFullYear()
+// console.log(theyear)
+// document.querySelector(".h6a").textContent=theyear;
+
+let clok=()=>{
+setInterval(clok, 1000);
+    let time=new Date();
+    let hr=time.getHours();
+    let min=time.getMinutes();
+    let sec=time.getSeconds();
+    am_pm="AM";
+
+    if(hr >=12){
+        hr -=12;
+        am_pm="PM";
+    }
+    if(hr>12){
+        am_pm="AM";
+    }
+    
+    currentTime=
+    hr  +
+    ":" +
+    min +
+    ":" +
+    sec +
+    am_pm;
+    
+    document.getElementById(
+        "digitalwatch"
+    ).innerHTML=currentTime;
+}
+clok();
+
+// function dateTime(){
+//     let date=new Date();
+//     // let Date=('1','2','3','4','5','6','7','8','9',
+//     // '10','11','12','13','14','15','16','17','18','19',
+//     // '20','21','22','23','24','25','26','27','28','29','30','31');
+//     let days = ('Sunday','Monday','Tuesday','Wednesday','Thirsday',
+//     'Friday','Saturday');
+//     let months=('Jan','Feb','Mar','Apr','May','June','July',
+//     'Aug','Sep','Oct','Nov','Dec');
+//     let day=days>date.getDay();
+//     let month=months>date.getMonth();
+//     let year=date.getFullYear();
+
+//     dateTime=
+//     day +
+//     "," +
+//     month +
+//     "," +
+//     year ;
+
+//     document.getElementById(
+//         "DateTime"
+//     ).innerHTML=dateTime;
+// }
+// dateTime();
+
